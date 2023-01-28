9afade99f91175ed2da63ad529d0e3e8ebb1f449
rizalfahmi302 <rizalfahmi302@gmail.com>
2023-01-28 17:48:50 +0700

    feat: add loadenv configuration and connection to database

--- config/config.go
+++ config/config.go
@@ -1,1 +1,95 @@
 package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	JWTKey                 string
+	CloudinaryName         string
+	CloudinaryApiKey       string
+	CloudinaryApiScret     string
+	CloudinaryUploadFolder string
+)
+
+type AppConfig struct {
+	DBUser string
+	DBPass string
+	DBHost string
+	DBPort int
+	DBName string
+}
+
+func LoadEnv() *AppConfig {
+	var appConfig AppConfig
+
+	isRead := true
+
+	if val, found := os.LookupEnv("DB_USER"); found {
+		appConfig.DBUser = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DB_PASS"); found {
+		appConfig.DBPass = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DB_HOST"); found {
+		appConfig.DBHost = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DB_PORT"); found {
+		cnv, _ := strconv.Atoi(val)
+		appConfig.DBPort = cnv
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DB_NAME"); found {
+		appConfig.DBName = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("JWT_KEY"); found {
+		JWTKey = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("CLOUDINARY_CLOUD_NAME"); found {
+		CloudinaryName = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("CLOUDINARY_API_KEY"); found {
+		CloudinaryApiKey = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("CLOUDINARY_API_SECRET"); found {
+		CloudinaryApiScret = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("CLOUDINARY_UPLOAD_FOLDER"); found {
+		CloudinaryUploadFolder = val
+		isRead = false
+	}
+	
+	if isRead {
+		if err := godotenv.Load("app.env"); err != nil {
+			log.Println("Load env failed", err)
+			return nil
+		}
+
+		appConfig.DBUser = os.Getenv("DB_USER")
+		appConfig.DBName = os.Getenv("DB_NAME")
+		appConfig.DBPass = os.Getenv("DB_PASS")
+		appConfig.DBHost = os.Getenv("DB_HOST")
+		readDBPort := os.Getenv("DB_PORT")
+		appConfig.DBPort, _ = strconv.Atoi(readDBPort)
+
+		JWTKey = os.Getenv("JWT_KEY")
+
+		CloudinaryName = os.Getenv("CLOUDINARY_CLOUD_NAME")
+		CloudinaryApiKey = os.Getenv("CLOUDINARY_API_KEY")
+		CloudinaryApiScret = os.Getenv("CLOUDINARY_API_SECRET")
+		CloudinaryUploadFolder = os.Getenv("CLOUDINARY_UPLOAD_FOLDER")
+	}
+	return &appConfig
+}
--- config/db.go
+++ config/db.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func OpenDB(cfg *AppConfig) *gorm.DB {
+	var db *gorm.DB
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		cfg.DBUser,
+		cfg.DBPass,
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBName,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("database connection error", err.Error())
+		return nil
+	}
+	return db
+}
