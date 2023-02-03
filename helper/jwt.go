965cde8d84d3f003661f5f09efc3fba7bde7c4b9
rizalfahmi302 <rizalfahmi302@gmail.com>
2023-02-04 03:32:29 +0700

    feat: add function generate jwt token

--- helper/jwt.go
+++ helper/jwt.go
@@ -1,1 +1,23 @@
 package helper
+
+import (
+	"log"
+	"time"
+	"yuhuuuMit/config"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func TokenGenerate(id uint) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorizad"] = true
+	claims["userID"] = id
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix() // token expired
+	tokenHash := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenHash.SignedString([]byte(config.JWTKey))
+	if err != nil {
+		log.Println("error generate token jwt ~", err)
+		return "", err
+	}
+	return token, nil
+}
