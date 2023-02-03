9b26b1ef3d3744e28a8ac9139127530f43bf9078
rizalfahmi302 <rizalfahmi302@gmail.com>
2023-02-04 03:38:55 +0700

    feat: add login response struct

--- feature/user/handler/response.go
+++ feature/user/handler/response.go
@@ -1,1 +1,13 @@
 package handler
+
+type LoginRes struct {
+	ID          uint   `json:"id" form:"id"`
+	Email       string `json:"email" form:"email"`
+	Fullname    string `json:"fullname" form:"fullname"`
+	Username    string `json:"username" form:"username"`
+	Gender      string `json:"gender" form:"gender"`
+	Avatar      string `json:"avatar" form:"avatar"`
+	Sampul      string `json:"sampul" form:"sampul"`
+	DateOfBirth string `json:"dateofbirth" form:"dateofbirth"`
+	Bio         string `json:"bio" form:"bio"`
+}
