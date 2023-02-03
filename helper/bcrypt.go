7ad54ff001e0a2b7d0d1c21d4ab6abd0f2abdd4f
rizalfahmi302 <rizalfahmi302@gmail.com>
2023-02-04 03:37:11 +0700

    feat: add bcrypt for compare password

--- helper/bcrypt.go
+++ helper/bcrypt.go
@@ -9,8 +9,16 @@ import (
 func PassBcrypt(pass string) (string, error) {
 	hashed, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
 	if err != nil {
-		log.Println("error bcrypt", err)
+		log.Println("error password generate bcrypt ~", err)
 		return "", err
 	}
 	return string(hashed), nil
+}
+
+func PassCompare(dbPass, inputPass string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(dbPass), []byte(inputPass)); err != nil {
+		log.Println("error password compare bcrypt ~", err)
+		return err
+	}
+	return nil
 }
