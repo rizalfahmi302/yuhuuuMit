5b2215d150b4a1da4dc01785fd90abac0d469c46
rizalfahmi302 <rizalfahmi302@gmail.com>
2023-02-04 00:08:12 +0700

    feat: add function for register user

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"log"
+	"yuhuuuMit/config"
+	userHandler "yuhuuuMit/feature/user/handler"
+	userRepository "yuhuuuMit/feature/user/repository"
+	userService "yuhuuuMit/feature/user/service"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	c := config.LoadEnv()    // panggil fungsi baca env
+	db := config.OpenDB(c)   // panggil fungsi konek ke database
+	config.GormMigration(db) // auto migration
+
+	v := validator.New()
+
+	userRep := userRepository.New(db)
+	userSrv := userService.New(userRep, v)
+	userHdl := userHandler.New(&userSrv)
+
+	e := echo.New()
+
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format:           "${time_custom}, method=${method}, uri=${uri}, status=${status}\n",
+		CustomTimeFormat: "2006-01-02 15:04:05",
+	}))
+
+	e.POST("/register", userHdl.RegisterHdl())
+
+	if err := e.Start(":8000"); err != nil {
+		log.Fatal(err)
+	}
+}
