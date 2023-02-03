5ca9a3ffaea1903f9a0e69b697889f42f985a8ae
rizalfahmi302 <rizalfahmi302@gmail.com>
2023-02-04 00:04:54 +0700

    feat: add register request

--- feature/user/handler/request.go
+++ feature/user/handler/request.go
@@ -1,1 +1,9 @@
 package handler
+
+type RegisterReq struct {
+	Fullname string `json:"fullname" form:"fullname"`
+	Username string `json:"username" form:"username"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+	Gender   string `json:"gender" form:"gender"`
+}
