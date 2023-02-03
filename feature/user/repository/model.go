622aee26ecfb09023a45a8e541a50b6015975168
rizalfahmi302 <rizalfahmi302@gmail.com>
2023-02-04 00:06:16 +0700

    feat: add User struct & convert Core to User

--- feature/user/repository/model.go
+++ feature/user/repository/model.go
@@ -1,1 +1,35 @@
 package repository
+
+import (
+	"yuhuuuMit/feature/user"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Fullname    string
+	Username    string `gorm:"unique"`
+	Email       string `gorm:"unique"`
+	Password    string 
+	Gender      string 
+	Avatar      string
+	Sampul      string
+	DateOfBirth string
+	Bio         string
+}
+
+func CoreToUser(data user.Core) User {
+	return User{
+		Model:       gorm.Model{ID: data.ID},
+		Fullname:    data.Fullname,
+		Username:    data.Username,
+		Email:       data.Email,
+		Password:    data.Password,
+		Gender:      data.Gender,
+		Avatar:      data.Avatar,
+		Sampul:      data.Sampul,
+		DateOfBirth: data.DateOfBirth,
+		Bio:         data.Bio,
+	}
+}
